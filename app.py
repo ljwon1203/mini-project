e9903bc051d6554b9fee7f0caba697260de3fdb6
mitoconcrete <xogns98@gmail.com>
2022-11-02 15:02:29 +0900

    api 결정.

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ def members():
 API 목록
 '''
 # 1. comments GET
-@app.route("결정해야함", methods=["GET"])
+@app.route("/comments", methods=["GET"])
 def get_comments():
     comment_list = list(db.comments.find({}, {'_id': False}))
     return jsonify({'comments': comment_list})
 
 # 2. comment POST
-@app.route("결정해야함", methods=["POST"])
+@app.route("/comments", methods=["POST"])
 def post_comments():
     name_receive = request.form['name_give']
     comment_receive = request.form["comment_give"]
@@ -43,7 +43,7 @@ def post_comments():
     return jsonify({'msg': '등록 완료!'})
 
 # 3. comments DELETE
-@app.route("결정해야함", methods=["POST"])
+@app.route("/comments/delete", methods=["POST"])
 def delete_comments():
     name_receive = request.form['name_give']
     doc.comments.delete_one({"name": name_receive})
