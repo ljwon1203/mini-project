d5c0a9aff5809d6422a1e1e70b9e2e2723179580
mitoconcrete <xogns98@gmail.com>
2022-11-02 14:11:58 +0900

    코멘트 해제.

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 
-# client = MongoClient('각자의 DB주소')
-# db = client.dbsparta
+client = MongoClient('각자의 DB주소')
+db = client.dbsparta
 
 '''
 페이지 템플릿 렌더링 기능
@@ -22,32 +22,32 @@ def members():
 '''
 API 목록
 '''
-# # 1. comments GET
-# @app.route("결정해야함", methods=["GET"])
-# def get_comments():
-#     comment_list = list(db.comments.find({}, {'_id': False}))
-#     return jsonify({'comments': comment_list})
+# 1. comments GET
+@app.route("결정해야함", methods=["GET"])
+def get_comments():
+    comment_list = list(db.comments.find({}, {'_id': False}))
+    return jsonify({'comments': comment_list})
 
-# # 2. comment POST
-# @app.route("결정해야함", methods=["POST"])
-# def post_comments():
-#     name_receive = request.form['name_give']
-#     comment_receive = request.form["comment_give"]
+# 2. comment POST
+@app.route("결정해야함", methods=["POST"])
+def post_comments():
+    name_receive = request.form['name_give']
+    comment_receive = request.form["comment_give"]
 
-#     doc = {
-#         'name': name_receive,
-#         'comment': comment_receive
-#     }
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive
+    }
 
-#     doc.comments.insert_one(doc)
-#     return jsonify({'msg': '등록 완료!'})
+    doc.comments.insert_one(doc)
+    return jsonify({'msg': '등록 완료!'})
 
-# # 3. comments DELETE
-# @app.route("결정해야함", methods=["POST"])
-# def delete_comments():
-#     name_receive = request.form['name_give']
-#     doc.comments.delete_one({"name": name_receive})
-#     return jsonify({'msg': '삭제 완료!'})
+# 3. comments DELETE
+@app.route("결정해야함", methods=["POST"])
+def delete_comments():
+    name_receive = request.form['name_give']
+    doc.comments.delete_one({"name": name_receive})
+    return jsonify({'msg': '삭제 완료!'})
 
 
 if __name__ == '__main__':
